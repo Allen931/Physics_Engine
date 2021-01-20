4dfe701b86cd9e3d8ff140214ddfe8246a09cecc
Hanpei Fang <hanpeifang@ruri.waseda.jp>
2021-01-21 04:11:47 +0800

    Rotation Matrix

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,3 +1,5 @@
+import org.junit.experimental.max.MaxCore;
+
 public class Matrix {
     private final Vector columnVector0, columnVector1;
 
@@ -11,22 +13,29 @@ public class Matrix {
         this.columnVector1 = columnVector1;
     }
 
+    public Matrix(double angle) {
+        double sin = Math.sin(angle);
+        double cos = Math.cos(angle);
+        columnVector0 = new Vector(cos, sin);
+        columnVector1 = new Vector(-sin, cos);
+    }
+
     public Matrix transpose() {
         Vector rowVector0 = new Vector(columnVector0.getX(), columnVector1.getX());
         Vector rowVector1 = new Vector(columnVector0.getY(), columnVector1.getY());
         return new Matrix(rowVector0, rowVector1);
     }
 
-    public Vector multiplyWithVector(Vector vector) {
+    public Vector transform(Vector vector) {
         Vector v0 = columnVector0.multiply(vector.getX());
         Vector v1 = columnVector1.multiply(vector.getY());
 
         return new Vector(v0.getX() + v1.getX(), v0.getY() + v1.getY());
     }
 
-    public Matrix multiplyWithMatrix(Matrix matrix) {
-        Vector v0 = multiplyWithVector(matrix.columnVector0);
-        Vector v1 = multiplyWithVector(matrix.columnVector1);
+    public Matrix transform(Matrix matrix) {
+        Vector v0 = transform(matrix.columnVector0);
+        Vector v1 = transform(matrix.columnVector1);
         return new Matrix(v0, v1);
     }
 
